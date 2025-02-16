5bc9476e73f225e2b4138db7e41412341716c1ea
Iswed <milanastasenko5@gmail.com>
2025-02-16 20:53:52 +0300

    change

--- src/main/java/servlet/MainServlet.java
+++ src/main/java/servlet/MainServlet.java
@@ -1,2 +1,17 @@
-package servlet;public class MainServlet {
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String value = req.getParameter("key");
+        if(value == null) value = "";
+        resp.getWriter().println(value);
+    }
 }
